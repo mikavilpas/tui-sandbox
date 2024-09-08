370a1614de7787b19cc5db6f2d37a4c9c64b14aa
Mika Vilpas <mika.vilpas@gmail.com>
2024-09-08 08:27:56 +0300

    refactor: move more app code away from the library (#10)

--- packages/integration-tests/library/server/types.ts
+++ packages/integration-tests/library/server/types.ts
@@ -1,6 +1,3 @@
-import { z } from "zod"
-import { MyTestDirectoryContentsSchema, testDirectoryFiles } from "../../MyTestDirectory"
-
 /** Describes the contents of the test directory, which is a blueprint for
  * files and directories. Tests can create a unique, safe environment for
  * interacting with the contents of such a directory.
@@ -14,18 +11,3 @@ export type TestDirectory = {
   rootPathAbsolute: string
   contents: object
 }
-
-/** The arguments given from the tests to send to the server */
-export const myStartNeovimArguments = z.object({
-  filename: z
-    .union([
-      testDirectoryFiles,
-      z.object({
-        openInVerticalSplits: z.array(testDirectoryFiles),
-      }),
-    ])
-    .optional(),
-  startupScriptModifications: z
-    .array(z.enum(MyTestDirectoryContentsSchema.shape["config-modifications"].shape.contents.keyof().options))
-    .optional(),
-})

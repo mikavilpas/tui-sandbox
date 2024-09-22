86c09fb75b732dcb66390e6d129b1222801fb3af
Mika Vilpas <mika.vilpas@gmail.com>
2024-09-22 19:22:10 +0300

    feat: allow test dirs that have no contents

---
+++ packages/library/src/server/neovim/environment/createTempDir.test.ts
@@ -0,0 +1,34 @@
+import fs, { rmdirSync } from "fs"
+import nodePath from "path"
+import { expect, it } from "vitest"
+import type { TestDirsPath } from "./createTempDir"
+import { createTempDir } from "./createTempDir"
+
+type TestTempDirPrefix = "test-temp-dir-"
+
+class TempDirectory implements Disposable {
+  constructor(public readonly path: string) {}
+
+  public static create(): TempDirectory {
+    const tmp = fs.mkdtempSync("test-temp-dir-" satisfies TestTempDirPrefix)
+    return new TempDirectory(tmp)
+  }
+
+  [Symbol.dispose](): void {
+    rmdirSync(this.path, { recursive: true })
+  }
+}
+
+it("should create a temp dir with no contents", async () => {
+  // typically the user will want to have contents, but this should not be an error
+  await using dir = TempDirectory.create()
+  const result = await createTempDir({
+    testEnvironmentPath: dir.path,
+    outputFilePath: nodePath.join(dir.path, "MyTestDirectory.ts"),
+  })
+
+  expect(result.contents).toEqual({})
+  expect(result.testEnvironmentPath).toEqual(dir.path)
+  expect(result.testEnvironmentPath.startsWith("test-temp-dir-" satisfies TestTempDirPrefix)).toBeTruthy()
+  expect(result.testEnvironmentPathRelative.startsWith("testdirs" satisfies TestDirsPath)).toBeTruthy()
+})

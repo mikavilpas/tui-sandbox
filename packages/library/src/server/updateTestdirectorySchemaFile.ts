62681186ab60802983e3b3efbe2670785243bf34
Mika Vilpas <mika.vilpas@gmail.com>
2024-10-23 21:55:45 +0300

    test: updateTestdirectorySchemaFile (#88)

--- packages/library/src/server/updateTestdirectorySchemaFile.ts
+++ packages/library/src/server/updateTestdirectorySchemaFile.ts
@@ -7,11 +7,13 @@ export type TestServerConfig = {
   outputFilePath: string
 }
 
+export type UpdateTestdirectorySchemaFileResult = "updated" | "did-nothing"
+
 export async function updateTestdirectorySchemaFile({
   testEnvironmentPath,
   outputFilePath,
-}: TestServerConfig): Promise<void> {
-  const newSchema = await buildTestDirectorySchema(testEnvironmentPath)
+}: TestServerConfig): Promise<UpdateTestdirectorySchemaFileResult> {
+  const newSchema: string = await buildTestDirectorySchema(testEnvironmentPath)
   let oldSchema = ""
 
   try {
@@ -25,5 +27,8 @@ export async function updateTestdirectorySchemaFile({
     // because file watchers will trigger on file changes and we don't want to
     // trigger a build if the schema hasn't changed
     writeFileSync(outputFilePath, newSchema)
+    return "updated"
+  } else {
+    return "did-nothing"
   }
 }

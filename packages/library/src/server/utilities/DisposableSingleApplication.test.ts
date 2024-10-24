609a5f27d0cc1c0a1b98030156ba4acd1b587ef3
Mika Vilpas <mika.vilpas@gmail.com>
2024-10-24 21:50:00 +0300

    refactor: make DisposableSingleApplication more testable (#91)
    ...and test it

---
+++ packages/library/src/server/utilities/DisposableSingleApplication.test.ts
@@ -0,0 +1,70 @@
+import type { StartableApplication } from "./DisposableSingleApplication"
+import { DisposableSingleApplication } from "./DisposableSingleApplication"
+
+vi.spyOn(console, "log").mockImplementation(vi.fn())
+
+class TestDisposableSingleApplication extends DisposableSingleApplication {
+  public getApplication() {
+    return this.application
+  }
+}
+
+const fakeApp: StartableApplication = {
+  processId: 123,
+  write: vi.fn(),
+  killAndWait: vi.fn(),
+}
+
+describe("DisposableSingleApplication", () => {
+  it("has no application when created", () => {
+    const app = new TestDisposableSingleApplication()
+    expect(app.processId()).toBeUndefined()
+    expect(app.getApplication()).toBeUndefined()
+  })
+
+  it("can start an application", async () => {
+    const app = new TestDisposableSingleApplication()
+    await app.startNextAndKillCurrent(async () => fakeApp)
+    expect(app.processId()).toBe(123)
+    expect(app.getApplication()).toBe(fakeApp)
+  })
+
+  it("can write to the application", async () => {
+    const app = new TestDisposableSingleApplication()
+    await app.startNextAndKillCurrent(async () => fakeApp)
+    await app.write("hello")
+    expect(fakeApp.write).toHaveBeenCalledWith("hello")
+  })
+
+  it("fails to write if the application is not started", async () => {
+    // there is no need to support soft failing in the ui, so we do hard
+    // failing to make this error obvious
+    const app = new TestDisposableSingleApplication()
+    await expect(app.write("hello")).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[AssertionError: The application not started yet. It makes no sense to write to it, so this looks like a bug.]`
+    )
+  })
+
+  describe("disposing", () => {
+    it("disposes the application when disposed", async () => {
+      // it's important to make sure there are no dangling applications when
+      // starting new tests or ending the user session entirely and closing the
+      // application
+      const app = new TestDisposableSingleApplication()
+
+      await app.startNextAndKillCurrent(async () => fakeApp)
+      expect(app.getApplication()).toBe(fakeApp)
+
+      await app[Symbol.asyncDispose]()
+      expect(fakeApp.killAndWait).toHaveBeenCalledOnce()
+    })
+
+    it("does nothing if there is no application to dispose", async () => {
+      const app = new TestDisposableSingleApplication()
+      expect(app.getApplication()).toBeUndefined()
+      expect(app.processId()).toBeUndefined()
+
+      expect(() => app[Symbol.asyncDispose]()).not.toThrow()
+    })
+  })
+})
--- packages/library/src/server/utilities/DisposableSingleApplication.ts
+++ packages/library/src/server/utilities/DisposableSingleApplication.ts
@@ -1,29 +1,38 @@
+import assert from "assert"
 import type { TerminalApplication } from "./TerminalApplication"
+
+export type StartableApplication = Pick<TerminalApplication, "write" | "processId" | "killAndWait">
 
 /** A testable application that can be started, killed, and given input. For a
  * single instance of this interface, only a single instance can be running at
- * a time (1 to 1 mapping).
+ * a time.
  */
-export abstract class DisposableSingleApplication implements AsyncDisposable {
-  protected application: TerminalApplication | undefined
+export class DisposableSingleApplication implements AsyncDisposable {
+  protected application: StartableApplication | undefined
 
-  public async killCurrent(): Promise<void> {
-    await this.application?.killAndWait()
+  public async startNextAndKillCurrent(startNext: () => Promise<StartableApplication>): Promise<void> {
+    await this[Symbol.asyncDispose]()
+    this.application = await startNext()
   }
 
   public async write(input: string): Promise<void> {
-    return this.application?.write(input)
+    assert(
+      this.application,
+      "The application not started yet. It makes no sense to write to it, so this looks like a bug."
+    )
+    this.application.write(input)
   }
 
   public processId(): number | undefined {
     return this.application?.processId
   }
 
-  async [Symbol.asyncDispose](): Promise<void> {
+  /** Kill the current application if it exists. */
+  public async [Symbol.asyncDispose](): Promise<void> {
     if (this.processId() === undefined) {
       return
     }
     console.log(`Killing current application ${this.processId()}...`)
-    await this.killCurrent()
+    await this.application?.killAndWait()
   }
 }

1c4d9194ec8961bde0e8f84d500c27363c66a61b
Mika Vilpas <mika.vilpas@gmail.com>
2024-11-03 22:07:32 +0200

    feat(wip): allow access to neovim via a type safe socket connection (#109)
    This change is a bit of work-in-progress effort. It does not actually
    expose the client yet. I'll have to think how it could be done, since we
    have to run the tests in the browser, but the client exists on the
    backend.
    
    Right now I want to roll this out to see if causes any stability issues.
    It's lazy so it should basically do nothing right now.

---
+++ packages/library/src/server/neovim/NeovimJavascriptApiClient.test.ts
@@ -0,0 +1,42 @@
+import { access } from "fs/promises"
+import { attach } from "neovim"
+import type { PollingInterval } from "./NeovimJavascriptApiClient"
+import { connectNeovimApi } from "./NeovimJavascriptApiClient"
+
+vi.mock("neovim")
+vi.mock("fs/promises")
+
+const mocked = {
+  attach: vi.mocked(attach),
+  access: vi.mocked(access),
+  log: vi.spyOn(console, "log").mockImplementation(() => {
+    //
+  }),
+}
+const pollingInterval = 100 satisfies PollingInterval
+
+beforeEach(() => {
+  vi.useFakeTimers()
+})
+
+afterEach(() => {
+  vi.useRealTimers()
+})
+
+it("is lazy - does not connect right away", async () => {
+  mocked.access.mockRejectedValue(new Error("no such file or directory"))
+  connectNeovimApi("foosocket")
+
+  vi.advanceTimersByTime(pollingInterval)
+  expect(mocked.attach).not.toHaveBeenCalled()
+})
+
+it("connects right away if the socket file is already there", async () => {
+  mocked.access.mockResolvedValue(undefined)
+  const lazyClient = connectNeovimApi("foosocket")
+  await lazyClient.get()
+
+  vi.advanceTimersByTime(pollingInterval)
+  expect(mocked.attach).toHaveBeenCalledWith({ socket: "foosocket" })
+  expect(mocked.attach).toHaveBeenCalledTimes(1)
+})
---
+++ packages/library/src/server/neovim/NeovimJavascriptApiClient.ts
@@ -0,0 +1,27 @@
+import { access } from "fs/promises"
+import type { NeovimClient as NeovimApiClient } from "neovim"
+import { attach } from "neovim"
+import { Lazy } from "../utilities/Lazy"
+
+export type NeovimJavascriptApiClient = NeovimApiClient
+
+export type PollingInterval = 100
+
+export function connectNeovimApi(socketPath: string): Lazy<Promise<NeovimJavascriptApiClient>> {
+  // it takes about 100ms for the socket file to be created - best make this
+  // Lazy so that we don't wait for it unnecessarily.
+  return new Lazy(async () => {
+    for (let i = 0; i < 100; i++) {
+      try {
+        await access(socketPath)
+        console.log(`socket file ${socketPath} created after at attempt ${i + 1}`)
+        break
+      } catch (e) {
+        console.log(`polling for socket file ${socketPath} to be created (attempt ${i + 1})`)
+        await new Promise(resolve => setTimeout(resolve, 100 satisfies PollingInterval))
+      }
+    }
+
+    return attach({ socket: socketPath })
+  })
+}

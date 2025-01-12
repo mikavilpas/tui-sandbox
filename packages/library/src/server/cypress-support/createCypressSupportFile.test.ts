ecdb825231aec515ac1aa9c0bb057d6b057c4288
Mika Vilpas <mika.vilpas@gmail.com>
2025-01-12 18:10:44 +0200

    feat: create config-modifications directory if it doesn't exist

--- packages/library/src/server/cypress-support/createCypressSupportFile.test.ts
+++ packages/library/src/server/cypress-support/createCypressSupportFile.test.ts
@@ -1,15 +1,19 @@
 import { readFileSync, writeFileSync } from "fs"
+import { mkdir, stat } from "fs/promises"
 import { createCypressSupportFileContents } from "./contents.js"
 import type { CreateCypressSupportFileResult } from "./createCypressSupportFile.js"
 import { createCypressSupportFile } from "./createCypressSupportFile.js"
 
 vi.mock("fs")
+vi.mock("fs/promises")
 vi.mock("./contents.ts")
 
 const mocked = {
   readFileSync: vi.mocked(readFileSync),
   writeFileSync: vi.mocked(writeFileSync),
   createCypressSupportFileContents: vi.mocked(createCypressSupportFileContents),
+  mkdir: vi.mocked(mkdir),
+  stat: vi.mocked(stat),
 }
 
 describe("createCypressSupportFileContents", () => {
@@ -40,5 +44,25 @@ describe("createCypressSupportFileContents", () => {
     })
 
     expect(result).toBe("did-nothing" satisfies CreateCypressSupportFileResult)
+  })
+
+  it("should create the config-modifications directory if it does not exist", async () => {
+    // This directory is required to exist so that a type safe schema of the
+    // neovimArguments can be built and used.
+    mocked.readFileSync.mockImplementationOnce(() => "")
+    mocked.writeFileSync.mockImplementationOnce(() => {
+      //
+    })
+    mocked.createCypressSupportFileContents.mockImplementationOnce(async () => "contents")
+    mocked.stat.mockRejectedValueOnce(new Error("ENOENT"))
+
+    const result = await createCypressSupportFile({
+      cypressSupportDirectoryPath: "cypress/support",
+      supportFileName: "tui-sandbox.ts",
+    })
+
+    expect(result).toBe("updated" satisfies CreateCypressSupportFileResult)
+    expect(mocked.mkdir).toHaveBeenCalledWith("cypress/support/config-modifications")
+    expect(mocked.mkdir).toHaveBeenCalledTimes(1)
   })
 })

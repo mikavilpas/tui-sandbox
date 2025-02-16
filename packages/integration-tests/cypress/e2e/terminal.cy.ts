b0c374eb995e68b04d7bc9002a064a1f824c532b
Mika Vilpas <mika.vilpas@gmail.com>
2025-02-16 09:04:27 +0200

    fix: terminal starting in the blueprint directory

--- packages/integration-tests/cypress/e2e/terminal.cy.ts
+++ packages/integration-tests/cypress/e2e/terminal.cy.ts
@@ -1,3 +1,5 @@
+import type { TestDirsPath } from "@tui-sandbox/library/src/server/neovim/environment/createTempDir"
+
 describe("TerminalTestApplication features", () => {
   it("can start the shell", () => {
     cy.visit("/")
@@ -10,6 +12,12 @@ describe("TerminalTestApplication features", () => {
       // verify that the shell is able to run a simple command
       cy.typeIntoTerminal(`echo "42 + 1" | bc{enter}`)
       cy.contains("43")
+
+      // the shell should have been started in the createUniqueDirectory path,
+      // not the test directory blueprint which includes the testdirs
+      // directory
+      cy.typeIntoTerminal("ls -al{enter}")
+      cy.contains("testdirs" satisfies TestDirsPath).should("not.exist")
     })
   })
 

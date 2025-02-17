7d24167bbb533026ad2efc1596994e4b35ff2325
Mika Vilpas <mika.vilpas@gmail.com>
2025-02-17 19:48:11 +0200

    chore: enable experimentalRunAllSpecs in cypress.config.ts

--- packages/integration-tests/cypress.config.ts
+++ packages/integration-tests/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
+    experimentalRunAllSpecs: true,
     baseUrl: `http://localhost:5173`,
     retries: {
       runMode: 2,

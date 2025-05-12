e476dd3b46d2751ae967c9078fafeb0916036803
Mika Vilpas <mika.vilpas@gmail.com>
2025-05-12 21:25:04 +0300

    refactor: reuse RgbColor type in color-utilities

--- packages/library/src/client/color-utilities.test.ts
+++ packages/library/src/client/color-utilities.test.ts
@@ -1,8 +1,9 @@
+import type { RgbColor } from "./color-utilities.js"
 import { rgbify } from "./color-utilities.js"
 
 describe("rgbify", () => {
   it("converts a catppuccin RGB color to a CSS color string", () => {
-    const color = { r: 1, g: 2, b: 3 }
+    const color = { r: 1, g: 2, b: 3 } satisfies RgbColor
     expect(rgbify(color)).toEqual("rgb(1, 2, 3)")
   })
 })

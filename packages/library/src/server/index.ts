0a106f0df83d2a81730f0e2144fa0c728db0f36d
Mika Vilpas <mika.vilpas@gmail.com>
2025-05-18 18:12:26 +0300

    feat(utils): export the Lazy utility class

--- packages/library/src/server/index.ts
+++ packages/library/src/server/index.ts
@@ -3,4 +3,5 @@
 export { startTestServer } from "./server.js"
 export { updateTestdirectorySchemaFile } from "./updateTestdirectorySchemaFile.js"
 export type { TestServerConfig } from "./updateTestdirectorySchemaFile.js"
+export { Lazy } from "./utilities/Lazy.js"
 import "core-js/proposals/async-explicit-resource-management.js"

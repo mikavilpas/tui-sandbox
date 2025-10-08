45cb9e0a9b41a4a5fae6eebabd237995c2e53352
Mika Vilpas <mika.vilpas@gmail.com>
2025-10-08 17:41:41 +0300

    refactor: remove non-useful comments

--- packages/library/src/scripts/parseArguments.ts
+++ packages/library/src/scripts/parseArguments.ts
@@ -3,7 +3,6 @@ import * as z from "zod"
 
 export const parseArguments = async (args: string[]): Promise<ParseArgumentsResult | undefined> => {
   {
-    // tui neovim prepare
     const schema = z.tuple([z.literal("neovim"), z.literal("prepare")])
     const prepareArguments = schema.safeParse(args)
     if (prepareArguments.success) {
@@ -14,7 +13,6 @@ export const parseArguments = async (args: string[]): Promise<ParseArgumentsResu
   }
 
   {
-    // tui neovim exec <command>
     const schema = z.tuple([z.literal("neovim"), z.literal("exec"), z.string()])
     const execArguments = schema.safeParse(args)
     if (execArguments.success) {
@@ -28,7 +26,6 @@ export const parseArguments = async (args: string[]): Promise<ParseArgumentsResu
   }
 
   {
-    // tui start
     const schema = z.tuple([z.literal("start")])
     const result = schema.safeParse(args)
     if (result.success) {
@@ -39,7 +36,6 @@ export const parseArguments = async (args: string[]): Promise<ParseArgumentsResu
   }
 
   {
-    // tui start
     const schema = z.tuple([z.literal("run")])
     const result = schema.safeParse(args)
     if (result.success) {

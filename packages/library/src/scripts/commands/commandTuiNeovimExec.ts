4407ff878b7680446bff02d917f745d6a79fff03
Mika Vilpas <mika.vilpas@gmail.com>
2025-10-11 09:49:28 +0300

    feat: validate NVIM_APPNAME in commandTuiNeovimExec
    Closes https://github.com/mikavilpas/tui-sandbox/issues/616

--- packages/library/src/scripts/commands/commandTuiNeovimExec.ts
+++ packages/library/src/scripts/commands/commandTuiNeovimExec.ts
@@ -10,12 +10,25 @@ export async function commandTuiNeovimExec(command: NeovimExec, config: TestServ
     console.log(`  neovim output: ${data}`)
   })
   const testDirectory = await prepareNewTestDirectory(config.config)
+  const NVIM_APPNAME = process.env["NVIM_APPNAME"]
+
+  if (NVIM_APPNAME && !config.config.integrations.neovim.NVIM_APPNAMEs.find(n => n === NVIM_APPNAME)) {
+    process.exitCode = 1
+    const message = `The NVIM_APPNAME environment variable is set to "${NVIM_APPNAME}", but only the following neovim configurations are known in the configuration file: ${JSON.stringify(
+      config.config.integrations.neovim.NVIM_APPNAMEs
+    )}. Please set NVIM_APPNAME to one of the configured names or unset it to use the default ("nvim"). Config file path: ${config.configFilePath}`
+
+    console.error(message)
+
+    return
+  }
+
   await app.startNextAndKillCurrent(
     testDirectory,
     {
       filename: "empty.txt",
       headlessCmd: command.command,
-      NVIM_APPNAME: process.env["NVIM_APPNAME"],
+      NVIM_APPNAME,
     },
     { cols: 80, rows: 24 }
   )

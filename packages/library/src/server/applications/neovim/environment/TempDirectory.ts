ea244de661cc9dc6b89ddef66b5ad015972528f4
Mika Vilpas <mika.vilpas@gmail.com>
2026-06-19 16:34:02 +0300

    chore: allow reusing TempDirectory for tests
    The class is very reusable now that we allow customizing the unique
    prefix. It will be very useful for tests.

--- packages/library/src/server/applications/neovim/environment/TempDirectory.ts
+++ packages/library/src/server/applications/neovim/environment/TempDirectory.ts
@@ -6,8 +6,8 @@ export type TestTempDirPrefix = "test-temp-dir-"
 export class TempDirectory implements Disposable {
   private constructor(public readonly path: string) {}
 
-  public static create(): TempDirectory {
-    const tmp = fs.mkdtempSync("test-temp-dir-" satisfies TestTempDirPrefix)
+  public static create(prefix: `${string}-` = "test-temp-dir-" satisfies TestTempDirPrefix): TempDirectory {
+    const tmp = fs.mkdtempSync(prefix)
     const absolutePath = nodePath.resolve(tmp)
     return new TempDirectory(absolutePath)
   }

96f61ae860b56533b73d661b8cac8280e01776e3
Mika Vilpas <mika.vilpas@gmail.com>
2026-09-16 17:43:01 +0300

    chore: remove old `allowCypressEnv` setting (deprecated)

--- packages/integration-tests/cypress.config.ts
+++ packages/integration-tests/cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
-    allowCypressEnv: false,
     experimentalRunAllSpecs: true,
     baseUrl: `http://localhost:5173`,
     retries: {
